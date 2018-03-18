cf2d8fb240389b0c4f0cfd70b4435ba0682a8b43
kavii <kavita.kat963@gmail.com>
2018-03-18 15:14:23 +0530

    firstpus

---
+++ demo.py
@@ -0,0 +1,30 @@
+#myvar="any name"
+#print(myvar)
+#str1="john"
+#str2="Doe"
+#print("Hello  {} {} ".format(str1,str2))
+#print(f"Hello {str1} {str2}")
+#var="kavitaKatdare@yahoo.in"
+#print(var[:-9])
+#print(var[:10])
+
+#name =input("Enter your name:")
+#print(f"Hello {name}")
+#name =input("Enter your name:")
+#print(f"Hello {name}")
+
+#str1=input("Enter ur num:")
+#print(f"length of ur num is {int(len(str1))} and square is {int(str1**2)}")
+
+#LIST
+#mylist = [1,2,3,4]
+#print(mylist[1])
+
+#a="00000000000hello"
+#print(a.lstrip('0'))
+
+#mystr ="s1 s2 s3 s4"
+#print(mystr.split(" "))
+
+mystr="s1 s2 s3 s4"
+('-').join(mystr)
